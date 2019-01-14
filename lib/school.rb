2e0b576c73a652f2398f4fffa0e62967226b2ed5
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 00:35:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,8 +13,11 @@ class School
   end
   
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    if @roster[grade] = []
+      @roster[grade] == name
+    else 
+      @roster[grade] << name
+    end
     @roster
   end
   
