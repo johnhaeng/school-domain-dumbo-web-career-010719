0e7e9161493d8ada3599bc4f48573277aee1b7bb
Jun Haeng Lee <johnhaeng@gmail.com>
2019-01-14 00:31:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+
+class School
+  attr_accessor :name, :grade, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+  
+  def add_student(name, grade)
+    hash = {}
+    hash[grade] = @roster
+    hash[grade] << @roster.push(name)
+    hash
+  end
+  
+end
